36392a58076f7e632dbe91c50b361f137a1c4f6d
Kedanco <kelvinchowjunjie120@hotmail.com>
2017-09-19 16:31:50 +0800

    created tweet

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,43 @@
+class TweetsController < ApplicationController
+
+  def home
+
+  end
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    tweet_params = params[:tweet].permit(:text,:user_id)
+    user = User.find(tweet_params[:user_id])
+    @tweet = user.tweets.new(tweet_params)
+    if @tweet.save
+      flash[:success] = "Tweet successfully created!"
+      redirect_to user_path(user.id)
+    else
+      flash[:danger] = "There was an error saving your tweet."
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
