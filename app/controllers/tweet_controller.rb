244df56306bc74fddce932e3a8aa7150d100d8eb
Kedanco <kelvinchowjunjie120@hotmail.com>
2017-09-19 15:55:28 +0800

    added tweet model/database, migration required

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,34 @@
+class TweetController < ApplicationController
+
+  def home
+
+  end
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
